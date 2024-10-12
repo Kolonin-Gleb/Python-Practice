d2045505fb99d302487d7e4ed128fb44e8d27c53
w80cI0-liA <topsecretway@yandex.ru>
2024-10-12 12:59:26 +0300

    Level 3 completed

--- MISIS labs/Lab2/Lab2 levels_1_2_3.py
+++ MISIS labs/Lab2/Lab2 levels_1_2_3.py
@@ -73,9 +73,11 @@ for x in x_values:
 '''
 
 
-################################## LEVEL 2
+################################## LEVEL 2 and LEVEL 3
+# Уровень 3 = Дополнить задачи из LEVEL 2 вводом данных до момента,
+# когда пользователь решит остановиться.
 
-# Task 2
+# Task 2 (level 3 done)
 '''
 # Функция для проверки, лежит ли точка внутри круга
 def is_point_in_circle(x, y, a, b, r):
@@ -89,43 +91,51 @@ def is_point_in_circle(x, y, a, b, r):
 a = float(input("Введите координату a (x центра круга): "))
 b = float(input("Введите координату b (y центра круга): "))
 r = float(input("Введите радиус круга r: "))
-n = int(input("Введите количество точек: "))
+# n = int(input("Введите количество точек: "))
 
 # Инициализация счетчика
 inside_circle = 0
 
 # Ввод координат n точек
-for i in range(n):
-    x = float(input(f"Введите координату x для точки {i + 1}: "))
-    y = float(input(f"Введите координату y для точки {i + 1}: "))
-    
+# for i in range(n):
+exit = 0
+while exit != 1:
+    # print(f"Ввод координат точки {i+1}")
+    print("Ввод координат точки")
+    x = float(input(f"Введите координату x для точки: "))
+    y = float(input(f"Введите координату y для точки: "))
     # Проверяем, попадает ли точка в круг
     if is_point_in_circle(x, y, a, b, r):
         # print(f"Точка ({x}, {y}) лежит внутри круга.")
         inside_circle += 1
     # else:
         # print(f"Точка ({x}, {y}) лежит вне круга.")
+    exit = int(input("Введите 1 для прекращения ввода и 0 для продлолжения: "))
 
 
 # Сколько точек попало в круг с радиусом r с центром в точке (a; b)
 print(f"Количество точек, попавших в круг: {inside_circle}")
 '''
 
-# Task 5
+# Task 5 (level 3 done)
 '''
 standard = 60 # Пусть норматив - пробежать за 60 секунд
 passed = 0 # Прошедших норматив
 
 print("Введите результаты спортсменов: ")
-for i in range(30):
-    result = float(input(f"Спортсмен {i + 1} пробежал за секунд = "))
+# for i in range(30):
+exit = 0
+while exit != 1:
+    # result = float(input(f"Спортсмен {i + 1} пробежал за секунд = "))
+    result = float(input(f"Спортсмен пробежал за секунд = "))
     if result <= standard:
         passed += 1
+    exit = int(input("Введите 1 для прекращения ввода и 0 для продлолжения: "))
 
 print(f"Количество спортсменов, выполнивших норматив: {passed}")
 '''
 
-# Task 8
+# Task 8 (level 3 done)
 '''
 def get_closest_point_data(points):
     closest_point_index = None
@@ -141,13 +151,20 @@ def get_closest_point_data(points):
     return closest_point_index, min_distance
 
 
-n = int(input("Введите количество точек: "))
+# n = int(input("Введите количество точек: "))
+n = 0
 
 points = []
-for i in range(n):
-    x = float(input(f"Введите координату x для точки {i+1}: "))
-    y = float(input(f"Введите координату y для точки {i+1}: "))
+# for i in range(n):
+exit = 0
+while exit != 1:
+    # x = float(input(f"Введите координату x для точки {i+1}: "))
+    x = float(input(f"Введите координату x для точки: "))
+    # y = float(input(f"Введите координату y для точки {i+1}: "))
+    y = float(input(f"Введите координату y для точки: "))
     points.append((x, y)) # Сохранение данных точки в виде кортежа в список
+
+    exit = int(input("Введите 1 для прекращения ввода и 0 для продлолжения: "))
 
 # Поиск и получение данных ближайшей точки
 index, distance = get_closest_point_data(points)
@@ -157,43 +174,41 @@ print(f"Точка под номером {index + 1} является ближа
 print(f"Расстояние до начала координат: {distance:.2f}")
 '''
 
-# Task 11
+# Task 11 (level 3)
 '''
-'''
-
 # Получение данных группы
 # (Кол. неуспевающих и средний балл)
 def get_group_data(students):
     failing_students = 0
-    grades_sum = 0
-
+    grades_sum = 0 # Сумма оценок студентов группы
     for grades in students:
         student_avg_grade = sum(grades) / 4 # / 4 Ведь у каждого 4 оценки
-
         # Студентов с средним баллом < 3, считаем неуспевающими
         if student_avg_grade < 3:
             failing_students += 1
-
         grades_sum += sum(grades)
 
     # Средний балл группы = все оценки делить на их количество
     group_avg = grades_sum / (len(students)*4)
-
     return failing_students, group_avg
 
 
-
-n = int(input("Введите количество студентов: "))
+# n = int(input("Введите количество студентов: "))
 
 # Ввод оценок студентов
 students = []
-for i in range(n):
-    print(f"Введите 4 оценки для студента {i+1}:")
+exit = 0
+# for i in range(n):
+while exit != 1:
+    # print(f"Введите 4 оценки для студента {i+1}:")
+    print(f"Введите 4 оценки для студента:")
     grades = []
     for j in range(4):
         grade = float(input(f"Оценка {j+1}: "))
         grades.append(grade)
     students.append(grades)
+
+    exit = int(input("Введите 1 для прекращения ввода и 0 для продлолжения: "))
 
 # Рассчёт неуспевающих студентов
 failing_students, group_average = get_group_data(students)
@@ -201,9 +216,5 @@ failing_students, group_average = get_group_data(students)
 # Вывод результатов
 print(f"Число неуспевающих студентов: {failing_students}")
 print(f"Средний балл группы: {group_average:.2f}")
-
-
-################################## LEVEL 3
-# Дополнить задачи из LEVEL 2 вводом данных до момента,
-# когда пользователь решит остановиться.
+'''
 
